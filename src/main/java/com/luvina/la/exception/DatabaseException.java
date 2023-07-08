23ad29ba9ec2b2f2d62e00a8b477f2d8fdf7dae6
nvduc1510 <nvduc15102001@gmail.com>
2023-07-08 17:23:49 +0700

    an

--- src/main/java/com/luvina/la/exception/DatabaseException.java
+++ src/main/java/com/luvina/la/exception/DatabaseException.java
@@ -4,7 +4,6 @@
  * DatabaseException.java, July 5, 2023 nvduc
  */
 package com.luvina.la.exception;
-
 public class DatabaseException extends RuntimeException {
     public DatabaseException(String message) {
         super(message);
